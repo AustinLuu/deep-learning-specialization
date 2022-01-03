0df64c4dc8a4093ecebf12c0250f18c5916d9ddb
Austin Luu <32989647+AustinLuu@users.noreply.github.com>
2022-01-03 18:30:44 -0500

    c2 week 3 ass

--- Improving Deep Neural Networks/test.py
+++ Improving Deep Neural Networks/test.py
@@ -1,3 +1,19 @@
 import numpy as np
 import tensorflow as tf
 
+w = tf.Variable(0, dtype=tf.float32) # creating a variable w
+cost = tf.add(tf.add(w**2, tf.multiply(-10.0, w)), 25.0) # can be written as this - cost = w**2 - 10*
+train = tf.optimizers.SGD(learning_rate=0.1).minimize(cost, w,tape=tf.GradientTape())
+
+init = tf.global_variables_initializer()
+session = tf.Session()
+session.run(init)
+session.run(w) # Runs the definition of w, if you print this it will print zero
+session.run(train)
+
+print("W after one iteration:", session.run(w))
+
+for i in range(1000):
+    session.run(train)
+
+print("W after 1000 iterations:", session.run(w))
